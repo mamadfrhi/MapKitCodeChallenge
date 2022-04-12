5b6cb680af798bc6b31f69f131de068fd31a030d
Mamad Farrahi <mamad.frhi@gmail.com>
2022-04-12 09:50:19 +0200

    MapCarVC implemented.

--- Cars Map/Scenes/MapCars/ViewController/MapCarsVC.swift
+++ Cars Map/Scenes/MapCars/ViewController/MapCarsVC.swift
@@ -7,4 +7,41 @@
 
 import UIKit
 
-class MapCarsVC: UIViewController {}
+class MapCarsVC: UIViewController {
+    
+    // MARK: Outlets
+    
+    
+    // MARK: Properties
+    var viewModel: MapCarsViewModel! {
+        didSet {
+            viewModel.viewDelegate = self
+        }
+    }
+    
+    // MARK: UIViewController
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewModel.start()
+    }
+    
+    // MARK: Setup
+    
+    // MARK: Actions
+    @IBAction func retry(_ sender: Any) {
+        
+    }
+}
+
+// MARK: - ViewModel Delegate
+extension MapCarsVC: MapCarsViewModelViewDelegate {
+    func refreshScreen(with cars: [Car]) {
+        print("refreshScreen functino in MapCarsVC received \(cars.count) cars")
+    }
+    
+    func selected(car: Car) {
+        print("selected function in MapCarsVC received \(car) cars")
+    }
+    
+    
+}
