bb556a29e49523191f4777abccc567634b51c673
Mamad Farrahi <mamad.frhi@gmail.com>
2022-04-13 18:58:45 +0200

    null state for car images handled on api request.

--- Cars Map/Scenes/ApiClient.swift
+++ Cars Map/Scenes/ApiClient.swift
@@ -64,16 +64,23 @@ struct CarsAPIError: Error {
 extension UIImageView {
     func downloaded(from url: URL, contentMode mode: ContentMode = .scaleAspectFit) {
         contentMode = mode
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard
-                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+        URLSession.shared.dataTask(with: url) {
+            data, response, error in
+            
+            if let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
                 let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
                 let data = data, error == nil,
-                let image = UIImage(data: data)
-            else { return }
-            DispatchQueue.main.async() { [weak self] in
-                self?.image = image
+                let image = UIImage(data: data){
+                DispatchQueue.main.async() {
+                    self.image = image
+                }
             }
+            else {
+                DispatchQueue.main.async {
+                    self.image = UIImage(named: "car")
+                }
+            }
+            
         }.resume()
     }
     func downloaded(from link: String, contentMode mode: ContentMode = .scaleAspectFit) {
